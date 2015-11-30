f5410c4c8137c23f052d9bddaa1b61de0dbcf7af
NachoTelematic <nachoarroyopons@gmail.com>
2015-11-30 17:24:20 +0100

    Web realease 0.0.1

---
+++ public/js/controllers/loginCtrl.js
@@ -0,0 +1,54 @@
+
+function loginController($scope, $http) {
+    $scope.newEmpresa = {};
+    $scope.newUser = {};
+    $scope.loginUser = {};
+    $scope.empresas = {};
+
+
+    // Función para registrar a una empresa
+    $scope.registrarEmpresa = function() {
+        $http.post('/empresa', $scope.newEmpresa)
+            .success(function(data) {
+                $scope.newEmpresa = {};
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // Función para registrar a un usuario
+    $scope.registrarUser = function() {
+        $http.post('/user', $scope.newUser)
+            .success(function(data) {
+                $scope.newUser = {};
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // Función para registrar a un usuario
+    $scope.login = function() {
+        console.log("bieen");
+        $http.post('/user/login', $scope.loginUser)
+            .success(function(data) {
+                $scope.loginUser = {};
+                location.href = 'views/main.html'
+                console.log("bieen");
+
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $http.get('/empresas').success(function(data) {
+
+        $scope.empresas = data;
+        console.log("Log Empresas" + data);
+    })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+}
