571c4f18bfb9491971ac58d4874a18d2f7a23712
NachoTelematic <nachoarroyopons@gmail.com>
2015-12-13 20:45:57 +0100

    Post de comentarios mas detalles

--- public/js/controllers/DetallesController.js
+++ public/js/controllers/DetallesController.js
@@ -1,13 +1,15 @@
 'use strict';
 
 
-MetronicApp.controller('DetallesController', function($scope, $http, $stateParams) {
+MetronicApp.controller('DetallesController', function($scope, $http, $stateParams, $state) {
     $scope.$on('$viewContentLoaded', function () {
         Metronic.initAjax(); // initialize core components
     });
 
 
     $scope.empresa = {};
+    $scope.newComment = {};
+
     $http.get('/empresa/' + $stateParams.empresa_id).success(function (data) {
 
             $scope.empresa = data;
@@ -16,5 +18,17 @@ MetronicApp.controller('DetallesController', function($scope, $http, $stateParam
         .error(function (data) {
             console.log('Error: ' + data);
         });
+
+    $scope.nuevoComentario = function() {
+        $http.post('/empresa/' + $stateParams.empresa_id + '/comment', $scope.newComment)
+            .success(function(data) {
+                $scope.newComment = {};
+                $state.go('detalles');
+
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
 
 });
